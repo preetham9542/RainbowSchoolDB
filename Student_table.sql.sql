07040a8d4dd9cc407d1a5cd846a7bf014bdaa78b
abc <abc@gmail.com>
2024-01-23 23:55:20 +0530

    updated

--- Student_table.sql.sql
+++ Student_table.sql.sql
@@ -1,8 +1,4 @@
--- Ensure that you are using the RainbowSchoolDB database
-USE RainbowSchoolDB;
-GO
 
--- Create the Student table
 CREATE TABLE Student (
     StudentID INT PRIMARY KEY,
     FirstName VARCHAR(50),
@@ -11,17 +7,17 @@ CREATE TABLE Student (
     -- Other columns as needed
 );
 
-
 -- Insert sample data into the Student table
 INSERT INTO Student (StudentID, FirstName, LastName, ClassID)
 VALUES
-    (1, 'John', 'Doe', 1),
-    (2, 'Jane', 'Smith', 2),
-    (3, 'Bob', 'Johnson', 1),
-    (4, 'Alice', 'Williams', 3),
-    (5, 'Charlie', 'Brown', 2),
-    (6, 'Eva', 'Davis', 1),
-    (7, 'Frank', 'Miller', 3),
-    (8, 'Grace', 'Wilson', 2),
-    (9, 'Henry', 'Jones', 1),
-    (10, 'Ivy', 'Clark', 3);
+    (1, 'Preetham', 'Thota', 1),
+    (2, 'Praneeth', 'Sai', 2),
+    (3, 'Dheeraj', 'Yama', 1),
+    (4, 'Srikanth', 'Pasula', 3),
+    (5, 'premsai', 'Thanuku', 2),
+    (6, 'Vamshi', 'Vemula', 1),
+    (7, 'Sai kumar', 'sollu', 3),
+    (8, 'sunny', 'shrinks', 2),
+    (9, 'Prasad', 'Shiv', 1),
+    (10, 'sonu', 'Nigam', 3);
+
